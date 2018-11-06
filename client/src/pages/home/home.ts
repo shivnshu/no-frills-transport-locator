0fcfb56259acbe3c65607e246195f57baf46d76c
raghav garg <rohanji97@gmail.com>
2018-11-07 01:19:08 +0530

    done

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import {OnInit, Component} from "@angular/core";
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, LoadingController,NavController, NavParams } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { HomeService } from './home.service';
 import {Http,Headers} from '@angular/http';
@@ -24,8 +24,9 @@ export class HomePage  {
   mobile:any;
   carnum:any;
   authForm: FormGroup;
+  loading:any;
   
-  constructor(public navCtrl: NavController, public navParams: NavParams,private geolocation: Geolocation,private homeService: HomeService,private http:Http, public formBuilder: FormBuilder,private storage: Storage ) {
+  constructor(private loadingCtrl: LoadingController, public navCtrl: NavController, public navParams: NavParams,private geolocation: Geolocation,private homeService: HomeService,private http:Http, public formBuilder: FormBuilder,private storage: Storage ) {
     this.geolocation.getCurrentPosition().then((resp) => {
       //console.log('lat');
       //console.log('long');
@@ -52,6 +53,10 @@ export class HomePage  {
             mess: ['', Validators.compose([])]
         });
 
+        this.loading = this.loadingCtrl.create({
+          content: 'Registering You!'
+        });
+
         
     
   }
@@ -62,8 +67,10 @@ export class HomePage  {
     console.log('ionViewDidLoad RocketsPage');
    
   }
+  
 
   SendLocation(){
+    this.loading.present();
     let headers=new Headers();
     headers.append('Access-Control-Allow-Origin','*');
     headers.append('Access-Control-Allow-Headers' , '*');
@@ -94,19 +101,22 @@ export class HomePage  {
       };
       
         
-          
+      
           
           this.http.post('http://'+val1+':'+val+'/add_new_transport',JSON.stringify(body),{headers:headers}).subscribe(data=>{
             var content=String(data.text());
             var matched="Signup failed. Try signing in."; 
             if(content==matched){
+              this.loading.dismiss();
               alert("Already registered!");
             }
             else{
               this.storage.set('mobile',this.mobile);
               this.storage.set('key',publicKey);
+              this.loading.dismiss();
               alert("Successfully registed!");
             }      
+            
       
           
           
